24c60425b81d3036248198ae97a7c3fa114ede23
Duncan Geere <radio.edit@gmail.com>
2022-08-24 11:23:29 +0200

    Bottom anchors (for mobile)

--- map.js
+++ map.js
@@ -106,7 +106,7 @@ map.on('click', 'schoolstreets', (e) => {
     // Zoom to location
     map.flyTo({ center: coordinates });
 
-    popup = new mapboxgl.Popup({ offset: 20, maxWidth: '340px' })
+    popup = new mapboxgl.Popup({ offset: 20, maxWidth: '340px', anchor: 'bottom' })
         .setLngLat(coordinates)
         .setHTML(html)
         .addTo(map);
